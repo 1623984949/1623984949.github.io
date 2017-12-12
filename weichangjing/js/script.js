640f02e6f0dd031efe215c21d7d9cd8baf6b72b7
1623984949 <1623984949@qq.com>
2017-12-12 16:22:21 +0800

    背景音乐

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -27,4 +27,21 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
+//背景音乐
+var mp3btn =document.querySelector("#mp3btn");
+var mp3 =document.querySelector("#mp3");
+mp3btn.addEventListener('click', function () {
+    if(mp3.paused){
+        mp3.play();
+        mp3btn.style.animationPlayState = 'running';
+    }else {
+        mp3.pause();
+        mp3btn.style.animationPlayState = 'paused';
+    }
+});
+
+
+
+
+
 
